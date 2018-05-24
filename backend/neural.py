2255cff397b94e976b04266bd2bc7a8b5323c2c3
wiradani <adhan432@gmail.com>
2018-05-24 21:50:06 +0700

    new predict

--- backend/neural.py
+++ backend/neural.py
@@ -6,47 +6,59 @@ import pandas
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 import os
+import sklearn
 
 
 
 #Read Data
 os.chdir('/home/adhan/Projek/sisdas-API/backend/temp')
-filename = open('diabetes.csv', 'r')
-names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age','Class']
+filename = open('datatomat.csv', 'r')
+names = ['b', 'g', 'r', 'h', 's', 'v', 'Class']
 dataframe = pandas.read_csv(filename, names=names)
 
-#Normalize Data
-# separate array into input and output components
-array = dataframe.values
-X = array[:,0:8]
-Y = array[:,8]
-scaler = Normalizer().fit(X)
-normalizedData = scaler.transform(X)
-# summarize transformed data
-numpy.set_printoptions(precision=3)
-#print(normalizedX[0:5,:])
+namafile = open('predict.csv', 'r')
+atribut = ['b', 'g', 'r', 'h', 's', 'v']
+dataPredict = pandas.read_csv(namafile, names=atribut)
+
 
 #Split Data
-train, test = train_test_split(dataframe, test_size=0.2)
+train, test = train_test_split(dataframe, test_size=0.8)
+
 
 trainx = train.drop('Class', axis=1)
-trainy = train.drop(train.columns[[0, 1, 2,3,4,5,6,7]], axis=1)
+trainy = train.drop(['b','g','r','h','s','v'], axis=1)
+
 
 testx = test.drop('Class', axis=1)
-testy = test.drop(test.columns[[0, 1, 2,3,4,5,6,7]], axis=1)
+testy = test.drop(['b','g','r','h','s','v'], axis=1)
+
+data = dataframe.drop('Class', axis=1)
+kelas = dataframe.drop(['b','g','r','h','s','v'], axis=1)
 
 
 #MLP CLassification
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                     hidden_layer_sizes=(5, 2), random_state=1)
-
+#clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
+clf = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+       beta_1=0.9, beta_2=0.999, early_stopping=False,
+       epsilon=1e-08, hidden_layer_sizes=(10, 7), learning_rate='adaptive',
+       learning_rate_init=0.001, max_iter=5000000000, momentum=0.9,
+       nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+       solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
+       warm_start=False)
 #train data
-clf.fit(trainx, trainy)
-
+#clf.fit(trainx, trainy)
+clf.fit(data,kelas)
 #predict data
 clf.predict(testx)
 
+
 #print score
-print("Training set score: %f" % clf.score(trainx, trainy))
-print("Test set score: %f" % clf.score(testx, testy))
-print(pandas.DataFrame(clf.predict_proba(testx), columns=clf.classes_))
+#print("Training set score: %f" % clf.score(trainx, trainy))
+#print("Test set score: %f" % clf.score(testx, testy))
+
+print("Training set score: %f" % clf.score(data, kelas))
+
+print("Data masuk kedalam kelas :%d"% clf.predict(dataPredict))
+
+#print(pandas.DataFrame(clf.predict_proba(testx), columns=clf.classes_))
+
---
+++ backend/readFolder.py
@@ -0,0 +1,115 @@
+import cv2
+import numpy as np
+
+import glob
+import csv
+
+def subgraygray (gray1, gray2):
+    #catatan ukuran gray 1 dan 2 harus sama dan inten sitas gray2 berupa 0 atau 255 (treshold)
+    row, col = gray2.shape
+    output = np.zeros((row,col,1), np.uint8)
+    for i in range(0,row):
+        for j in range(0,col):
+            if int(gray1[i,j])-int(gray2[i,j]) < 0 :
+                output.itemset((i,j,0),0)
+            else:
+                output.itemset((i,j,0),int(gray1[i,j])-int(gray2[i,j]))
+    return output
+
+def subrgbgray(rgb,treshold):
+    row, col , raw = rgb.shape
+    #print row*col
+    output = np.zeros((row,col,3), np.uint8)
+    for i in range(0,row):
+        for j in range(0,col):
+            if treshold[i,j] != 255:
+                output.itemset((i,j,0),0)
+                output.itemset((i,j,1),0)
+                output.itemset((i,j,2),0)
+            else:
+                output[i,j]=rgb[i,j]
+    return output
+
+imgnames=sorted(glob.glob('tomat1/*.JPG'))
+data=[]
+for imgname in imgnames:
+    kernel24 = np.ones((24, 24), np.uint8)
+    tomat = cv2.imread(imgname)
+    tomat = cv2.resize(tomat, (0,0), fx=0.1, fy=0.1)
+    cv2.imshow(imgname,tomat)
+    b,g,r = cv2.split( tomat )
+    tomat_segmented = cv2.subtract(r,g)
+    # ret, tomat_segmented = cv2.threshold(tomat_segmented, 63,255,cv2.THRESH_BINARY)
+    ret, tomat_segmented = cv2.threshold(tomat_segmented, 15,255,cv2.THRESH_BINARY)
+    tomat_segmented = cv2.morphologyEx(tomat_segmented, cv2.MORPH_CLOSE, kernel24)
+    tomat_segmented = subrgbgray(tomat, tomat_segmented)
+    cv2.imshow(imgname,tomat_segmented)
+    #HSV
+    rhsv = cv2.cvtColor(tomat_segmented, cv2.COLOR_BGR2HSV)
+
+    # h,s,v = cv2.split(rhsv)
+    # cv2.imshow(imgname+"HSV", s)
+    row, col, ch=tomat_segmented.shape
+
+    #RGB &HSV tiap pixel
+    blue=0
+    red=0
+    green=0
+    hue=0
+    sat=0
+    val=0
+    for i in range(0, row):
+        for j in range(0, col):
+            b, g, r = tomat_segmented[i,j]
+            h, s, v = rhsv[i,j]
+            blue=blue+b
+            green=green+g
+            red=red+r
+            hue=hue+h
+            sat=sat+s
+            val=val+v
+
+
+    b=blue/(row*col)
+    g=green/(row*col)
+    r=red/(row*col)
+    h=hue/(row*col)
+    s=sat/(row*col)
+    v=val/(row*col)
+    # print(b)
+    # print(g)
+    # print(r)
+    # x=[b,g,r]
+    x=[b,g,r,h,s,v]
+    data.append(x)
+
+
+
+    cv2.waitKey()
+
+# a = np.asarray([[b,g,r]])
+# a.tofile('datatomats.csv',sep=',',format='%10.5f')
+
+myFile=open('datatomats.csv','w', newline='')
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(data)
+
+
+#histogram dengan RGB
+# color = ('b','g','r')
+# for i,cols in enumerate(color):
+#     histr = cv2.calcHist([img],[i],None,[256],[0,256])
+#     print(cols)
+#     for j, his in enumerate(histr):
+#         print(j)
+#         print(histr[j])
+#     plt.plot(histr,color = cols)
+#     plt.xlim([0,256])
+# plt.show()
+# r,g,b=cv2.split(img)
+# print(r)
+# print(g)
+# print(b)
+
+cv2.destroyAllWindows()
