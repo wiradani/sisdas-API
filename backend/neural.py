ad07e19e2463b43fdbe426fc54c54b34b7b58191
wiradani <adhan432@gmail.com>
2018-05-29 15:16:26 +0700

    new html

--- backend/neural.py
+++ backend/neural.py
@@ -37,14 +37,21 @@ class Neural:
 
 
     #MLP CLassification
-    #clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
     clf = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
           beta_1=0.9, beta_2=0.999, early_stopping=False,
           epsilon=1e-08, hidden_layer_sizes=(10, 7), learning_rate='adaptive',
-          learning_rate_init=0.001, max_iter=5000000000, momentum=0.9,
+          learning_rate_init=0.001, max_iter=100000, momentum=0.9,
           nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
           solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
           warm_start=False)
+    # clf = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+    #       beta_1=0.9, beta_2=0.999, early_stopping=False,
+    #       epsilon=1e-08, hidden_layer_sizes=(10, 7), learning_rate='adaptive',
+    #       learning_rate_init=0.001, max_iter=5000000000, momentum=0.9,
+    #       nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+    #       solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
+    #       warm_start=False)
     #train data
     #clf.fit(trainx, trainy)
     clf.fit(data,kelas)
